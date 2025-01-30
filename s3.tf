f4043df725a852f3f90ddbfc7b8a489bedb14a0f
A.Geetha <65497643+Geetha-9@users.noreply.github.com>
2025-01-30 16:06:37 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -22,4 +22,12 @@ versioning_configuration {
 status = "Enabled"
 }
 }
+terraform {
+backend "s3" {
+region = "us-east-1"
+bucket = "geetha.devops.project.bucket"
+key = "prod/terraform.tfstate"
+}
+}
+
 
